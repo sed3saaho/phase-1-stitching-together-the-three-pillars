dd1954a0433d62e3e4d4fb3c84c19e15229a8347
Sedly <sedly.keith@student.moringaschool.com>
2024-04-11 22:19:09 +0300

    phase-1-stitching-together-the-three-pillars

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+
+const articleHearts = document.querySelectorAll('.like');
+
+
+function sendLikeToServer() {
+
+  console.log('Sending like to the server...');
+}
+
+
+function handleLikeButtonClick(event) {
+  const heart = event.target;
+  
+  if (heart.classList.contains('activated')) {
+    
+    heart.classList.remove('activated');
+    
+    sendLikeToServer();
+  } else {
+    
+    heart.classList.add('activated');
+    
+    sendLikeToServer();
+  }
+}
+
+
+articleHearts.forEach(heart => {
+  heart.addEventListener('click', handleLikeButtonClick);
+});
